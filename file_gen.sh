5da4ce0ca7eb97e74db5010b8628005f99e236e1
Kutub Gandhi <kksgandhi@gmail.com>
2021-12-07 21:26:31 -0500

    add file generator for testing

---
+++ file_gen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir testdir
+
+touch ./testdir/taxes.txt
+touch ./testdir/social_security_number.txt
+touch ./testdir/homework.py
+touch ./testdir/medical_report.pdf
+touch ./testdir/diary.md
+touch ./testdir/love_letter.docx
+touch ./testdir/important_email.txt
+touch ./testdir/test_answers.zip
+touch ./testdir/system32.dll
+touch ./testdir/receipt.txt
+touch ./testdir/DO_NOT_DELETE.TXT
+touch ./testdir/family_photos.zip
+touch ./testdir/last_will_and_testament.pdf
